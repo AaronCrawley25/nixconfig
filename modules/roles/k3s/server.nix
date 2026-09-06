c43f42a6695af4cb68d549453652b4582e8a6371
Aaron Crawley <aaron.crawley@outlook.com.au>
2026-09-06 21:27:44 +0800

    fix k3s server args

--- modules/roles/k3s/server.nix
+++ modules/roles/k3s/server.nix
@@ -14,9 +14,9 @@
 
       extraFlags = [
         "--etcd-expose-metrics"
-        "--kube-controller-manager-arg \"bind-address=0.0.0.0\""
-        "--kube-scheduler-arg \"bind-address=0.0.0.0\""
-        "--kube-proxy-arg \"metrics-bind-address=0.0.0.0\""
+        "--kube-controller-manager-arg=\"bind-address=0.0.0.0\""
+        "--kube-scheduler-arg=\"bind-address=0.0.0.0\""
+        "--kube-proxy-arg=\"metrics-bind-address=0.0.0.0\""
       ];
     };
   };
