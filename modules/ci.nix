b69a740fae16f319e699d9d65d150e8e7d72fc7b
Aaron Crawley <aaron.crawley@outlook.com.au>
2026-09-08 18:11:39 +0800

    ci: add some more packages to build/cache

--- modules/ci.nix
+++ modules/ci.nix
@@ -2,11 +2,24 @@
   # Generate Github Matrix
   flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix { checks = self.packages; };
 
-  # Build these large ahh packages
+  # Build some flake packages that aren't cached anywhere
   flake.packages."x86_64-linux".quickshell-niri = inputs.qml-niri.packages."x86_64-linux".quickshell;
   flake.packages."x86_64-linux".bezel = inputs.bezel.packages."x86_64-linux".default;
-  flake.packages."aarch64-linux".linux_rpi5 =
-    inputs.nixos-raspberrypi.packages."aarch64-linux".linux_rpi5;
   flake.packages."x86_64-linux".lzbt = inputs.lanzaboote.packages."x86_64-linux".lzbt;
   flake.packages."x86_64-linux".stub = inputs.lanzaboote.packages."x86_64-linux".stub;
+
+  flake.packages."aarch64-linux".linux_rpi5 =
+    inputs.nixos-raspberrypi.packages."aarch64-linux".linux_rpi5;
+  flake.packages."aarch64-linux".linuxPackages_rpi5 =
+    inputs.nixos-raspberrypi.packages."aarch64-linux".linuxPackages_rpi5;
+  flake.packages."aarch64-linux".raspberrypi-utils =
+    inputs.nixos-raspberrypi.packages."aarch64-linux".raspberrypi-utils;
+  flake.packages."aarch64-linux".raspberrypi-udev-rules =
+    inputs.nixos-raspberrypi.packages."aarch64-linux".raspberrypi-udev-rules;
+  flake.packages."aarch64-linux".raspberrypifw =
+    inputs.nixos-raspberrypi.packages."aarch64-linux".raspberrypifw;
+  flake.packages."aarch64-linux".libraspberrypi =
+    inputs.nixos-raspberrypi.packages."aarch64-linux".libraspberrypi;
+  flake.packages."aarch64-linux".raspberrypiWirelessFirmware =
+    inputs.nixos-raspberrypi.packages."aarch64-linux".raspberrypiWirelessFirmware;
 }
