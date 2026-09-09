fe25dcb8eaa3784044d915989fb82d1850ae7b0f
Aaron Crawley <aaron.crawley@outlook.com.au>
2026-09-09 18:50:51 +0800

    add tls san option to k3s nodes

--- modules/hosts/crawley-k8s/ikora/configuration.nix
+++ modules/hosts/crawley-k8s/ikora/configuration.nix
@@ -6,6 +6,9 @@
       services.k3s = {
         serverAddr = "https://10.9.8.7:6443";
         tokenFile = "/var/lib/rancher/k3s/token";
+        extraFlags = [
+          "--tls-san=10.9.8.80"
+        ];
       };
 
       system.stateVersion = "26.05";
